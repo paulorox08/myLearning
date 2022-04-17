770824213688f8289ad7dce9f64952c33cdca7c0
Paulo Adeva <paulo.adeva@gmail.com>
2022-04-17 23:15:51 +1000

    saving library

--- JS/objects/library/script.js
+++ JS/objects/library/script.js
@@ -1,8 +1,31 @@
+const table = document.querySelector('.table');
+const addButton = document.querySelector('.addButton');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
 let myLibrary = [];
 
-function Book() {
+function Movie(title, director, genre, runtime, date) {
+    this.title = title
+    this.director = director
+    this.genre = genre
+    this.runtime = runtime
+    this.date = date
 }
 
-function addBookToLibrary() {
-    
+function addMovieToLibrary() {
+
 }
+
+const m1 = new Movie("Batman Begins", "Christoper Nolan", "Supehero", "140min");
+
+addButton.addEventListener('click', addMovieToLibrary)
+
+addButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+

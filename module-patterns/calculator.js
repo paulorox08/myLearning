5c1d8074d19c5a58fa9a3e52a282cad15290affc
Paulo Adeva <paulo.adeva@gmail.com>
2022-05-01 01:01:01 +1000

    module patterns

---
+++ module-patterns/calculator.js
@@ -0,0 +1,17 @@
+const calculator = (() => {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+
+    return {
+        add,
+        sub,
+        mul,
+        div,
+    };
+})();
+
+calculator.add(3,5);
+calculator.sub(6,2);
+calculator.mul(14,5534);
